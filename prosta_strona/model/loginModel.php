97c64765bdac61f96f6f4f2163293dacae595921
BartekFo <72879738+BartekFo@users.noreply.github.com>
2021-02-15 15:04:22 +0100

    rozbudowa logowania do strony

--- prosta_strona/model/loginModel.php
+++ prosta_strona/model/loginModel.php
@@ -12,6 +12,42 @@ class loginModel
         $this->password = $value;
     }
 
+    public function connectDb()
+    {
+        $host = 'localhost';
+        $user = 'root';
+        $pwd = '';
+        $dbName = "prostastronadb";
+
+        $mysqli = new mysqli($host, $user, $pwd, $dbName);
+
+        if (mysqli_connect_errno()) {
+            printf('Błąd połączenia z bazą ', mysqli_connect_error());
+        } else {
+            return $mysqli;
+        }
+    }
+
+    public function getUser($login, $pwd)
+    {
+        $mysqli = $this->connectDb();
+        $sql = "SELECT email, password FROM users WHERE email=? AND password =?";
+        //? $hashedPassword = sha1($pwd);
+        // * opcja z użyciem bcryptu i z przepuszczenie hasła 11 razy przez ziarno
+        $options = [
+            'cost' => 11
+        ];
+        $hashedPassword = password_hash($pwd, PASSWORD_BCRYPT, $options);
+        // *----------------------------------------------------------------
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('ss', $login, $hashedPassword);
+        $stmt->execute();
+        if ($stmt->affected_rows == 0) {
+        }
+
+        $stmt->close();
+    }
+
     public function checkLogin()
     {
         if (($_POST['email'] == 'bartek@gmail.com') && ($_POST['pwd']) == 'bartek') {
