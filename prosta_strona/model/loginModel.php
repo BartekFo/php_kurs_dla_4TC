d942fd4a1f29ef8adb85f809178484a4be8b25fe
BartekFo <72879738+BartekFo@users.noreply.github.com>
2021-02-24 12:42:18 +0100

    Kończenie logowania i dodawania tokenów

--- prosta_strona/model/loginModel.php
+++ prosta_strona/model/loginModel.php
@@ -18,88 +18,160 @@ class loginModel
 
         if (mysqli_connect_errno()) {
             printf('Błąd połączenia z bazą ', mysqli_connect_error());
+            return null;
         } else {
             return $mysqli;
         }
     }
 
-    public function checkUser($login, $pwd)
-    {
-        $mysqli = $this->connectDb();
-        $sql = "SELECT email, password FROM users WHERE email = ?";
-        //? $hashedPassword = sha1($pwd);
-        //* 'apple' => "dauidhaidaiuhiua";
-        //* 'apple' + dadhaiudahiu => daihdbahjdbawdja;
-
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('s', $login);
-        $stmt->execute();
-        $stmt->bind_result($email, $password);
-        print($stmt->affected_rows);
-        printf("%s (%s)\n", $email, $password);
-        if ($stmt->affected_rows > 0) {
-            $stmt->fetch();
-            $isPasswordVerified = password_verify($pwd, $password);
-            if ($isPasswordVerified) {
-                $stmt->close();
-                return true;
-            } else {
-                $stmt->close();
-                return false;
-            }
-        } else {
-            $stmt->close();
-            return false;
-        }
-    }
-
     public function checkLogin()
     {
-        if ($this->checkUser($_POST['email'], $_POST['pwd'])) {
-            $token = bin2hex(random_bytes(16));
-            $this->setLogin($_POST['email']);
-            $this->setSession($this->email, $token);
-            if ($this->saveTokenToDb($token)) {
-                return true;
+        $con = $this->connectDb();
+        if (!is_null($con)) {
+            $sql = "SELECT idusers FROM users WHERE email = ?";
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param('s', $_POST['email']);
+            $stmt->execute();
+            $stmt->store_result();
+            if ($stmt->num_rows > 0) {
+                $stmt->bind_result($id);
+                $stmt->fetch();
+                $sql = "SELECT password FROM users WHERE idusers = ?";
+                $stmt->free_result();
+                $stmt = $con->prepare($sql);
+                $stmt->bind_param('s', $id);
+                $stmt->execute();
+                $stmt->store_result();
+                if ($stmt->num_rows() > 0) {
+                    $stmt->bind_result($password);
+                    $stmt->fetch();
+                    $stmt->free_result();
+                    if (password_verify($_POST['pwd'], $password)) {
+                        $stmt->close();
+                        $con->close();
+                        $this->setLogin($_POST['email']);
+                        $this->setToken($id);
+                        $this->setSession($id, $this->email, $this->token);
+                        return true;
+                    } else {
+                        $stmt->close();
+                        $con->close();
+                        return false;
+                    }
+                } else {
+                    $stmt->close();
+                    $con->close();
+                    return false;
+                }
             } else {
+                $con->close();
                 return false;
             }
         } else {
+            $con->close();
             return false;
         }
     }
 
-    public function setSession($email, $token)
+    public function setSession($id, $email, $token)
     {
+        $_SESSION['idu'] = $id;
         $_SESSION['email'] = $email;
         $_SESSION['token'] = $token;
     }
 
-    public function saveTokenToDb($token)
+    public function setToken($id)
     {
-        $mysqli = $this->connectDb();
-        $sql = "SELECT idusers FROM users where email = '" . $_SESSION['email'] . "'";
-        $result = $mysqli->query($sql) or die($mysqli->error);
-        if ($result->fetch_assoc()) {
-            $id = $result->fetch_assoc();
-        } else {
-            printf('błąd', $mysqli->error);
-        }
-
-        $date = new DateTime();
-        $timeStamp = $date->getTimestamp();
-
-        $sql = "INSERT INTO tokens VALUES (NULL, " . $id . ", " . $token . ", " . $timeStamp . ")";
-        $result = $this->connectDb()->query($sql);
+        $con = $this->connectDb();
+        $token = bin2hex(random_bytes(16));
+        $this->token = $token;
+        $date = date("Y-m-d H:i:s");
+        $sql = "INSERT INTO tokens VALUES (NULL,'" . $id . "' , '" . $token . "', '" . $date . "')";
+        echo $sql;
+        $result = $con->query($sql);
         if ($result) {
+            $con->close();
             return true;
         } else {
+            $con->close();
             return false;
         }
     }
 
+    public function getToken($id)
+    {
+        $con = $this->connectDb();
+        $sql = "SELECT token_value FROM tokens WHERE idusera = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('d', $id);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($token);
+        $stmt->fetch();
+        $stmt->free_result();
+        $con->close();
+        return $token;
+    }
+
+    public function getDateToken($id)
+    {
+        $con = $this->connectDb();
+        $sql = "SELECT token_date FROM tokens WHERE idusera = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('d', $id);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($date);
+        $stmt->fetch();
+        $stmt->free_result();
+        $con->close();
+        return $date;
+    }
+
+    // TODO: zrobić dwie metody update i delete token.
+    public function updateToken($id)
+    {
+        $con = $this->connectDb();
+        $sql = "UPDATE tokens SET token_date = ? WHERE idusera = ?";
+        $stmt = $con->prepare($sql);
+        $date = date("Y-m-d H:i:s");
+        $stmt->bind_param('sd', $date, $id);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows() > 0) {
+            $stmt->free_result();
+            $con->close();
+            return true;
+        } else {
+            $stmt->free_result();
+            $con->close();
+            return false;
+        }
+    }
+
+    public function deleteToken($id)
+    {
+        $con = $this->connectDb();
+        $sql = "DELETE FROM tokens WHERE idusera = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param('d', $id);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows() > 0) {
+            $stmt->free_result();
+            $con->close();
+            return true;
+        } else {
+            $stmt->free_result();
+            $con->close();
+            return false;
+        }
+    }
+
+
     public function logout()
     {
+        $this->deleteToken($_SESSION['idu']);
         session_destroy();
     }
 }
--- prosta_strona/view/loginView.php
+++ prosta_strona/view/loginView.php
@@ -8,7 +8,7 @@ class loginView extends view
         $this->set("error", "");
         $this->set("isLogin", false);
         if ($this->isLogged($auth)) {
-            $this->set("error", $auth->login);
+            $this->set("error", "Poprawnie zalogowano");
             $this->set("isLogin", true);
             $this->redirect('orders');
         } else {
